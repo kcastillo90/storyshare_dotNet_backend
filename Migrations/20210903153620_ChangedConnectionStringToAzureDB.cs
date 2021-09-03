7ef1260237b2f7fc2ac8095ec8ec1289fbbff7fa
Kyle Castillo <kyle.castillo90@gmail.com>
2021-09-03 11:52:49 -0400

    attempts at migrating to azure psql db failed

---
+++ Migrations/20210903153620_ChangedConnectionStringToAzureDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace storyshare_dotNet_backend.Migrations
+{
+    public partial class ChangedConnectionStringToAzureDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
